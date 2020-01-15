b9b129c5a48e0cad55c5db28bd5d98ca4dfa12de
guilherme <guischpor1@live.com>
2020-01-15 18:30:52 -0300

    e

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Editar <code>src/App.js</code> e salvar e carregar.
+          Editar <code>src/App.js</code> e salvar e carregar e.
         </p>
         <a
           className="App-link"
