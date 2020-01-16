5c17bcf8361653a643458f68900f1d4f63980939
guilherme <guischpor1@live.com>
2020-01-16 17:05:43 -0300

    teste

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom';
+import PrimeiroComponente from './components/PrimeiroComponente'
 
 const elemento = document.getElementById('root')
-ReactDOM.render(<h1>Olá React</h1>, elemento)
+ReactDOM.render(
+       <div>
+           <PrimeiroComponente />
+       </div>
+    ,elemento)
+
+
+
+
+
+
+//const jsx = <h1>Olá React!</h1>
+//ReactDOM.render(jsx, elemento)
